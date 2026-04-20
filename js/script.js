b1ba5248fdfa5f3182d4f5c1ed69b7a2504f6717
GabMatheus <gab_matheus@hotmail.com>
2026-04-20 19:52:47 -0300

    ajustes gerais do sistema

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-/* Acessa JSON para manipula-lo */ 
+/* ------------------------VARIÁVEIS GLOBAIS ------------------*/
 let dados = [];
+let mapa;
+let marker;
 
+/*----------------------- Carrega os dados do arquivo JSON ------------------------*/
 async function carregarDados() {
-  const res = await fetch("js/data.json");
-  dados = await res.json();
+    const res = await fetch("js/data.json");
+    dados = await res.json();
 }
 
-/* Tela Nossa História */
+/*----------------------------- Renderiza a página nossa história ------------------------*/
 async function renderSobreNos() {
     const main = document.getElementById("conteudo-principal");
     main.innerHTML = `
@@ -18,7 +21,7 @@ async function renderSobreNos() {
     `;
 }
 
-/* Tela Contato */
+/*---------------------- Renderiza a página de contato -----------------------*/
 async function renderContato() {
     const main = document.getElementById("conteudo-principal");
     main.innerHTML = `
@@ -34,62 +37,58 @@ async function renderContato() {
     `;
 }
 
-/*cria e carrega o mapa pela api do OpenStreetMap*/
-let mapa;
-let marker;
-
-function iniciarMapa() {
-  mapa = L.map('mapa').setView([-8.0476, -34.8770], 7);
-  marker = L.marker([-8.0476, -34.8770]).addTo(mapa);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
-  }).addTo(mapa);
-}
-
-/* mover mapa ao clicar nas unidades */
-function moverMapa(lat, lng) {
-  mapa.setView([lat, lng], 10);
-  marker.setLatLng([lat, lng]);
-}
-
-/* Renderiza a primeira tela dos cards das unidades */
+/*------------------- Renderiza a página inicial com os cards das unidades -----------------*/
 async function renderHome() {
-  await carregarDados();
-  const lista = document.getElementById("lista");
+    await carregarDados();
 
-  let html = "<h2>Selecione a unidade</h2>";
+    const lista = document.getElementById("lista");
+    let html = "<h2>Selecione a unidade</h2>";
 
-  dados.unidades.forEach(u => {
-
-    html += `
-        <div class="card-unidade" onclick="moverMapa(${u.coords.lat}, ${u.coords.lng}, '${u.nome}')">
-            <div class="info">
-                <h3>${u.nome}</h3>
-                <p>${u.endereco} - ${u.cidade}</p>
+    dados.unidades.forEach(u => {
+        html += `
+            <div class="card-unidade" onclick="moverMapa(${u.coords.lat}, ${u.coords.lng}, '${u.nome}')">
+                <div class="info">
+                    <h3>${u.nome}</h3>
+                    <p>${u.endereco} - ${u.cidade}</p>
+                </div>
+                <div class="foto">
+                    <img src="${u.foto}" alt="${u.nome}">
+                </div>
+                <button onclick="abrirCardapio(${u.id}); event.stopPropagation();">
+                    Ver Cardápio
+                </button>
             </div>
+        `;
+    });
 
-            <div class="foto">
-                <img src="${u.foto}" alt="${u.nome}">
-            </div>
-            <button onclick="abrirCardapio(${u.id}); event.stopPropagation();">
-                Ver Cardápio
-            </button>
-        </div>
-    `;
-  });
-
-  lista.innerHTML = html;
+    lista.innerHTML = html;
 }
 
-/* Renderiza a segunda tela do cardápio de acordo com a unidade q foi selecionada */
+/* ----------------------------segunda tela ------------------------------- */
 
 
-// Função para voltar inicio
+
+/*-------------- Inicializa o mapa com OpenStreetMap ----------------*/
+function iniciarMapa() {
+    mapa = L.map('mapa').setView([-8.0476, -34.8770], 7);
+    marker = L.marker([-8.0476, -34.8770]).addTo(mapa);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap'
+    }).addTo(mapa);
+}
+
+/*---------------- Move o mapa ---------------------*/
+function moverMapa(lat, lng) {
+    mapa.setView([lat, lng], 10);
+    marker.setLatLng([lat, lng]);
+}
+
+/*---------------- Volta para a tela inicial ---------------------*/
 function voltarParaHome() {
     const main = document.getElementById("conteudo-principal");
-    main.innerHTML = 
-    `<div id="lista"></div>
+    main.innerHTML = `
+        <div id="lista"></div>
         <div class="moldura-mapa">
             <div id="mapa"></div>
         </div>
@@ -99,7 +98,8 @@ function voltarParaHome() {
     renderHome();
 }
 
+/*---------------------- Inicializa o sistema  --------------------------*/
 document.addEventListener("DOMContentLoaded", () => {
-  iniciarMapa();
-  renderHome();
+    iniciarMapa();
+    renderHome();
 });
